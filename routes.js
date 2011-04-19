fe75eb0939713f0b17916a5da9ce4fe3ce864b86
Shawn Dellysse <sdellysse@gmail.com>
2011-04-18 21:17:17 -0400

    moved to prototypal inheritance

--- routes.js
+++ routes.js
@@ -1,53 +1,61 @@
-window.HashRegexRouter = new function () {
-  var self = this
-  var routes = _([])
-
-  self.match = function (route) {
-    if (typeof route === 'Array') {
-      _(route).each(function (r) {
-        self.match(r)
-      })
+window.HashRegexRouter = (function () {
+  var Singleton = function () {
+    this.routes = _([]);
+  };
+  Singleton.prototype.match = function (route_or_array) {
+    if (typeof route_or_array === 'Array') {
+      _(route_or_array).each(function (r) {
+        this.match(r);
+      });
     } else {
-      routes.push(route)
+      this.routes.push(route_or_array);
     }
 
-    return self
-  }
+    return this;
+  };
+  Singleton.prototype.hash_change_handler = function () {
+    var route_not_yet_found, hash;
 
-  self.start = function () {
-    var handler
+    route_not_yet_found = true;
+    hash = window.location.hash;
 
-    handler = function () {
-      var route_not_yet_found, hash
-
-      route_not_yet_found = true
-      hash = window.location.hash
-
-      routes.each(function (route) {
-        var matches, callback_arguments
-        if (route_not_yet_found) {
-          matches = hash.match(route.regex)
-          if (matches) {
-            callback_arguments = _(matches).rest()
-            if ('before' in route) {
-              route.before()
-            }
-            if ('around' in route) {
-              route.around(route.callback, _(matches).rest())
-            } else {
-              route.callback.apply(route.callback, _(matches).rest())
-            }
-            if ('after' in route) {
-              route.after()
-            }
-            route_not_yet_found = false;
+    this.routes.each(function (route) {
+      var matches, callback_arguments;
+      if (route_not_yet_found) {
+        matches = hash.match(route.regex);
+        if (matches) {
+          callback_arguments = _(matches).rest();
+          runnable_callback = function () {
+            var args = typeof arguments[0] === 'undefined' ? callback_arguments : arguments[0];
+            return route.callback.apply(route.callback, args);
+          };
+          if ('before' in route) {
+            route.before();
           }
+          if ('around' in route) {
+            route.around(route.callback, callback_arguments, runnable_callback);
+          } else {
+            route.callback.apply(route.callback, callback_arguments);
+          }
+          if ('after' in route) {
+            route.after();
+          }
+          route_not_yet_found = false;
         }
-      })
-    }
+      }
+    });
+  };
+  Singleton.prototype.start = function () {
+    var self;
+    self = this;
+
     jQuery(document).ready(function () {
-      jQuery(window).hashchange(handler)
-    })
-    handler();
-  }
-}
+      jQuery(window).hashchange(function () {
+        self.hash_change_handler();
+      });
+    });
+    this.hash_change_handler();
+  };
+
+  return new Singleton();
+})();
